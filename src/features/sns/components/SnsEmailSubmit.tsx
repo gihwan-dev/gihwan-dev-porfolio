fb24aa96ebd2d8d99af7bc70576ef8ebeb8f190f
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-22 21:43:16 +0900

    hotfix: mailto 버튼 에서 link 태그로 수정

--- src/features/sns/components/SnsEmailSubmit.tsx
+++ src/features/sns/components/SnsEmailSubmit.tsx
@@ -1,20 +1,15 @@
+import Link from 'next/link';
+
 const SnsEmailSubmit = () => {
   return (
-    <form
-      action={'mailto:rlghks3004@gmail.com'}
-      className={'flex w-full max-w-xl flex-col gap-4 p-2'}
-      method={'post'}
-      encType={'text/plain'}
+    <Link
+      href={'mailto:rlghks3004@gmail.com'}
+      className={
+        'mx-auto rounded-sm bg-text-primary-red px-8 py-3 font-bold text-white transition-all duration-300 hover:opacity-80'
+      }
     >
-      <button
-        type={'submit'}
-        className={
-          'w-full rounded-sm bg-text-primary-red py-3 font-bold text-white transition-all duration-300 hover:opacity-80'
-        }
-      >
-        Send email
-      </button>
-    </form>
+      Send email
+    </Link>
   );
 };
 
