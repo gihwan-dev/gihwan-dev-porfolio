f85a3753bcf25ae8b3eb5b0c83f020624556fa37
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-22 21:08:01 +0900

    fix: 폴더 위치 수정

--- src/app/main/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProjectsOverview } from '~/features/projects_overview';
-
-export const dynamic = 'force-dynamic';
-
-export default function ProjectPage() {
-  return <ProjectsOverview />;
-}
