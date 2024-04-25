a2d260de5851c56d6481091a745b1258990cc161
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-25 14:28:52 +0900

    feature(sentry): sentry 에러 로거 추가

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3a6b6990b68ba56303fe6ccbc3949c43@o4507145378594816.ingest.us.sentry.io/4507145380495360",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
