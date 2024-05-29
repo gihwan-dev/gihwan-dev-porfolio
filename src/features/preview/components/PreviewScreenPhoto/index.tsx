67eaf30c2758a075b02b9df605c654c6b5ee8075
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-29 20:15:41 +0900

    fix: 이미지 스타일 수정

--- src/features/preview/components/PreviewScreenPhoto/index.tsx
+++ src/features/preview/components/PreviewScreenPhoto/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Tabs, TabsList, TabsTrigger } from '~/components/ui/tabs';
 import ScreenMobileContent from './ScreenMobileContent';
 import ScreenTabletContent from './ScreenTabletContent';
@@ -11,8 +9,8 @@ interface Props {
 
 export default function PreviewScreenPhoto({ documentId }: Props) {
   return (
-    <div className="mx-auto my-8 h-[700] w-full max-w-5xl px-6 xl:px-12">
-      <h2 className={'my-12 text-2xl font-bold text-white'}>Screens</h2>
+    <div className="mx-auto my-8 box-border h-[45rem] w-full max-w-5xl rounded-md bg-main-foreground px-6 xl:px-12">
+      <h2 className={'py-8 text-2xl font-bold text-white'}>Screens</h2>
       <Tabs defaultValue={'mobile'}>
         <TabsList className={'flex w-full flex-row justify-center'}>
           <TabsTrigger value={'mobile'}>Mobile</TabsTrigger>
