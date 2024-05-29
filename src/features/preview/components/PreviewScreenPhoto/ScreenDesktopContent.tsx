ebd6cbe1c914623ff934e75be8faf348be95e4cc
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-29 19:59:02 +0900

    fix: 이미지 리스트 에러 수정

---
+++ src/features/preview/components/PreviewScreenPhoto/ScreenDesktopContent.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { Skeleton } from '~/components/ui/skeleton';
+import { Suspense } from 'react';
+import Image from 'next/image';
+import { TabsContent } from '~/components/ui/tabs';
+import { api } from '~/trpc/react';
+
+interface Props {
+  documentId: number;
+}
+
+export default function ScreenDesktopContent({ documentId }: Props) {
+  const { data, isLoading, isError, error } =
+    api.document.getDocumentScreenPhotos.useQuery({
+      documentId: documentId,
+      type: 'desktop',
+    });
+
+  if (isError) {
+    throw error;
+  }
+
+  let content = null;
+
+  if (!isLoading && (data?.length === 0 || !data)) {
+    content = (
+      <div
+        className={
+          'mt-4 flex h-full w-full items-center justify-center font-bold text-white'
+        }
+      >
+        <p>등록된 이미지가 없습니다!</p>
+      </div>
+    );
+  }
+
+  return (
+    <TabsContent
+      className={'flex h-full w-full flex-row overflow-visible overflow-x-auto'}
+      value={'desktop'}
+    >
+      {content}
+      {isLoading ? (
+        <div className={`mx-auto flex w-full max-w-5xl flex-row gap-4`}>
+          <Skeleton className={`aspect-desktop w-[300]`} />
+          <Skeleton className={`aspect-desktop w-[300]`} />
+          <Skeleton className={`aspect-desktop w-[300]`} />
+        </div>
+      ) : (
+        data?.map(screenImage => (
+          <Suspense
+            key={`screen-${screenImage.screen_image_id}`}
+            fallback={<Skeleton className={`aspect-desktop w-[300]`} />}
+          >
+            <div className={`aspect-desktop relative w-[300] shrink-0`}>
+              <Image
+                src={screenImage.url}
+                alt={`screen-${screenImage.screen_image_id}`}
+                fill
+                loading={'lazy'}
+                priority={false}
+                sizes={'300px'}
+                className={'z-20 object-contain'}
+              />
+            </div>
+          </Suspense>
+        ))
+      )}
+    </TabsContent>
+  );
+}
--- src/features/preview/components/PreviewScreenPhoto/index.tsx
+++ src/features/preview/components/PreviewScreenPhoto/index.tsx
@@ -1,90 +1,27 @@
 'use client';
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
-
-import { Suspense, useState } from 'react';
-import { api } from '~/trpc/react';
-import { Skeleton } from '~/components/ui/skeleton';
-import Image from 'next/image';
+import { Tabs, TabsList, TabsTrigger } from '~/components/ui/tabs';
+import ScreenMobileContent from './ScreenMobileContent';
+import ScreenTabletContent from './ScreenTabletContent';
+import ScreenDesktopContent from './ScreenDesktopContent';
 
 interface Props {
   documentId: number;
 }
 
-const aspectValue = {
-  mobile: 'aspect-mobile',
-  desktop: 'aspect-desktop',
-  tablet: 'aspect-tablet',
-};
-
 export default function PreviewScreenPhoto({ documentId }: Props) {
-  const [tabs, setTabs] = useState<'mobile' | 'desktop' | 'tablet'>('mobile');
-
-  const { data, isLoading, isError, error } =
-    api.document.getDocumentScreenPhotos.useQuery({
-      documentId: documentId,
-      type: tabs,
-    });
-
-  if (isError) {
-    throw error;
-  }
-
   return (
-    <div className="mx-auto my-8 w-full max-w-5xl px-6 xl:px-12">
+    <div className="mx-auto my-8 h-[700] w-full max-w-5xl px-6 xl:px-12">
       <h2 className={'my-12 text-2xl font-bold text-white'}>Screens</h2>
-      <Tabs
-        className={'flex w-full flex-col items-center justify-center'}
-        value={tabs}
-      >
-        <TabsList>
-          <TabsTrigger onClick={() => setTabs('mobile')} value={'mobile'}>
-            Mobile
-          </TabsTrigger>
-          <TabsTrigger onClick={() => setTabs('tablet')} value={'tablet'}>
-            Tablet
-          </TabsTrigger>
-          <TabsTrigger onClick={() => setTabs('desktop')} value={'desktop'}>
-            Desktop
-          </TabsTrigger>
+      <Tabs defaultValue={'mobile'}>
+        <TabsList className={'flex w-full flex-row justify-center'}>
+          <TabsTrigger value={'mobile'}>Mobile</TabsTrigger>
+          <TabsTrigger value={'tablet'}>Tablet</TabsTrigger>
+          <TabsTrigger value={'desktop'}>Desktop</TabsTrigger>
         </TabsList>
-        <TabsContent
-          className={'flex w-full flex-row gap-4 overflow-x-auto px-4 py-12'}
-          value={tabs}
-        >
-          {isLoading ? (
-            <div
-              className={`mx-auto my-8 flex w-full max-w-5xl flex-row gap-4 px-4 py-12 xl:px-12`}
-            >
-              <Skeleton className={`w-[300] ${aspectValue[tabs]}`} />
-              <Skeleton className={`w-[300] ${aspectValue[tabs]}`} />
-              <Skeleton className={`w-[300] ${aspectValue[tabs]}`} />
-            </div>
-          ) : (
-            data?.map(screenImage => (
-              <Suspense
-                key={`screen-${screenImage.screen_image_id}`}
-                fallback={
-                  <Skeleton className={`w-[300] ${aspectValue[tabs]}`} />
-                }
-              >
-                <div
-                  className={`${aspectValue[tabs]} relative w-[300] shrink-0`}
-                >
-                  <Image
-                    src={screenImage.url}
-                    alt={`screen-${screenImage.screen_image_id}`}
-                    fill
-                    loading={'lazy'}
-                    priority={false}
-                    sizes={'300px'}
-                    className={'z-20 object-contain'}
-                  />
-                </div>
-              </Suspense>
-            ))
-          )}
-        </TabsContent>
+        <ScreenMobileContent documentId={documentId} />
+        <ScreenTabletContent documentId={documentId} />
+        <ScreenDesktopContent documentId={documentId} />
       </Tabs>
     </div>
   );
